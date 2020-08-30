a04cd8453fcfcb7a667e8fa6f3d52de631530f0f
Martin Aarhus Gregersen <marting12@live.dk>
2020-08-30 16:50:59 +0200

    Updates application to suit my preferencees

--- template.rb
+++ template.rb
@@ -1,7 +1,7 @@
 =begin
-Template Name: Kickoff - Tailwind CSS
-Author: Andy Leverenz
-Author URI: https://web-crunch.com
+Template Name: My Rails template using Tailwind
+Author: Martin Aarhus Gregersen
+Author URI: https://mgregersen.dk
 Instructions: $ rails new myapp -d <postgresql, mysql, sqlite3> -m template.rb
 =end
 
@@ -10,10 +10,14 @@ def source_paths
 end
 
 def add_gems
+  gem 'administrate'
   gem 'devise', '~> 4.7', '>= 4.7.2'
+  gem 'devise_masquerade'
   gem 'friendly_id', '~> 5.3'
-  gem 'sidekiq', '~> 6.1', '>= 6.1.1'
   gem 'name_of_person', '~> 1.1', '>= 1.1.1'
+  gem 'noticed'
+  gem 'sidekiq', '~> 6.1', '>= 6.1.1'
+  gem 'sitemap_generator'
 end
 
 def add_users
@@ -37,6 +41,9 @@ def add_users
 
   # name_of_person gem
   append_to_file("app/models/user.rb", "\nhas_person_name\n", after: "class User < ApplicationRecord")
+
+  # Add Devise masqueradable to users
+  inject_into_file("app/models/user.rb", "masqueradable, :", after: "devise :")
 end
 
 def copy_templates
@@ -82,6 +89,43 @@ def add_sidekiq
   insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
 end
 
+def add_announcements
+  generate "model Announcement published_at:datetime announcement_type name description:text"
+  route "resources :announcements, only: [:index]"
+end
+
+def add_administrate
+  generate "administrate:install"
+
+  append_to_file "app/assets/config/manifest.js" do
+    "//= link administrate/application.css\n//= link administrate/application.js"
+  end
+
+  gsub_file "app/dashboards/announcement_dashboard.rb",
+    /announcement_type: Field::String/,
+    "announcement_type: Field::Select.with_options(collection: Announcement::TYPES)"
+
+  gsub_file "app/dashboards/user_dashboard.rb",
+    /email: Field::String/,
+    "email: Field::String,\n    password: Field::String.with_options(searchable: false)"
+
+  gsub_file "app/dashboards/user_dashboard.rb",
+    /FORM_ATTRIBUTES = \[/,
+    "FORM_ATTRIBUTES = [\n    :password,"
+
+  gsub_file "app/controllers/admin/application_controller.rb",
+    /# TODO Add authentication logic here\./,
+    "redirect_to '/', alert: 'Not authorized.' unless user_signed_in? && current_user.admin?"
+
+  environment do <<-RUBY
+    # Expose our application's helpers to Administrate
+    config.to_prepare do
+      Administrate::ApplicationController.helper #{@app_name.camelize}::Application.helpers
+    end
+  RUBY
+  end
+end
+
 def add_foreman
   copy_file "Procfile"
 end
@@ -99,6 +143,8 @@ after_bundle do
   add_users
   remove_app_css
   add_sidekiq
+  add_announcements
+  add_administrate
   add_foreman
   copy_templates
   add_tailwind
@@ -114,7 +160,7 @@ after_bundle do
   git commit: %Q{ -m "Initial commit" }
 
   say
-  say "Kickoff app successfully created! 👍", :green
+  say "App successfully initiated using template! 👍", :green
   say
   say "Switch to your app by running:"
   say "$ cd #{app_name}", :yellow
